9ecc1aa1be9e38bbb78ec314fc7aff6fda86051e
Amine <aminelahlou5@gmail.com>
2017-06-13 18:02:48 +0200

    [FIX] Renvoi d'un html au lieu d'un simple texte

--- 4-api.js
+++ 4-api.js
@@ -4,7 +4,7 @@ module.exports.getContent = function(cidTexte, callback) {
 	request(`https://www.legifrance.gouv.fr/affichTexte.do?cidTexte=${cidTexte}`, (error, response, body) => {
 		const $ = cheerio.load(body)
 		$('#exportRTF').remove()
-		const result = $('.data').text().replace(/(\n|\s|\r|\t)(\n|\s|\r|\t)+/g, '\n\n').trim()
+		const result = $('.data').html()
 		callback(result)
 	})
 }
